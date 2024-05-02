996fbafc415852101fa396e4348a5cfb928fbc57
ChuBe04 <vohongson8520@gmail.com>
2024-05-02 17:01:19 +0700

    DONE MINICART,SHOW MAIN CART

--- app/controller/client/CartController.js
+++ app/controller/client/CartController.js
@@ -1,7 +1,30 @@
+const Product = require('../../model/product.model');
+const { getAllBrands } = require('../../services/brand.services');
+const { getAllCategories } = require('../../services/category.services');
+
 class CartController {
   // [GET] /cart
-  mainCart(req, res) {
-    res.render('client/pages/cart.ejs');
+  async mainCart(req, res) {
+    // ? Get All Product and sort by purcharded desc
+    const getBestProductPurchased = await Product.find({})
+      .sort({ purchased: -1 })
+      .populate('category_id', 'name')
+      .limit(20);
+    // ? Get All Category
+    const getCategories = await getAllCategories();
+    // ? Get ALL Brand
+    const getBrands = await getAllBrands();
+    // ? Get All Carts
+    const carts = req.session.cart || [];
+    const userInfo = req.headers.authorization;
+    res.render('client/pages/cart.ejs', {
+      getBestProductPurchased,
+      getCategories,
+      getBrands,
+      userInfo,
+      slug: 'cart',
+      carts,
+    });
   }
 
   // [GET] /checkout
@@ -12,36 +35,75 @@ class CartController {
   // ? [GET] /addToCart
   addToCart(req, res) {
     const data = req.query;
-
-    if (
-      req.session.cart == null ||
-      req.session.cart == undefined ||
-      req.session.cart.length == 0
-    ) {
-      // ? If cart is empty, create new cart
-      req.session.cart = [];
-    }
-    const carts = req.session.cart;
-    // ? Check if product is already in cart
+    let carts = req.session.cart || [];
+    let check = false;
     if (carts.length > 0) {
       carts.forEach((cart) => {
         if (cart.id == data.id) {
           cart.quantity += 1;
-        } else {
-          carts.push({
-            id: data.id,
-            name: data.name,
-            price: data.price,
-            quantity: data,
-          });
+          cart.totalPrice = cart.quantity * cart.price;
+          check = true;
         }
       });
-    } else {
+    }
+
+    if (check == false) {
       carts.push({
         id: data.id,
         name: data.name,
-        price: data.price,
-        quantity: data.quantity,
+        slug: data.slug,
+        image: data.image,
+        price: parseInt(data.price),
+        quantity: parseInt(data.quantity),
+        totalPrice: parseInt(data.price) * parseInt(data.quantity),
+        type: data.type,
+      });
+    }
+
+    req.session.cart = carts;
+    let total = 0;
+    if (carts.length > 0) {
+      carts.forEach((cart) => {
+        total += parseInt(cart.totalPrice);
+      });
+    }
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Thêm vào giỏ hàng thành công!',
+      data: carts,
+      total: total,
+    });
+  }
+
+  // ? [GET] /getCart
+  getCart(req, res) {
+    let carts = req.session.cart || [];
+    let total = 0;
+    if (carts.length > 0) {
+      carts.forEach((cart) => {
+        total += cart.totalPrice;
+      });
+    }
+    res.status(200).json({
+      status: 'success',
+      data: carts,
+      total: total,
+    });
+  }
+
+  // ? [GET] /deleteItemCart
+  deleteItemCart(req, res) {
+    const { id } = req.params;
+    let carts = req.session.cart || [];
+    if (carts.length > 0) {
+      carts = carts.filter((cart) => cart.id != id);
+    }
+
+    let total = 0;
+    if (carts.length > 0) {
+      carts.forEach((cart) => {
+        total += cart.totalPrice;
       });
     }
 
@@ -49,8 +111,9 @@ class CartController {
 
     res.status(200).json({
       status: 'success',
-      message: 'Thêm vào giỏ hàng thành công!',
+      message: 'Xóa sản phẩm khỏi giỏ hàng thành công!',
       data: carts,
+      total: total,
     });
   }
 }
--- routes/client/cart.routes.js
+++ routes/client/cart.routes.js
@@ -1,13 +1,15 @@
 const express = require('express');
-const {
-  addToCart,
-  mainCart,
-} = require('../../app/controller/client/CartController');
+const CartController = require('../../app/controller/client/CartController');
 const CartRouter = express.Router();
 
 // ? [GET] /cart
-CartRouter.get('/', mainCart);
+CartRouter.get('/cart', CartController.mainCart);
 // ? [GET] /addToCart
-CartRouter.get('/addToCart', addToCart);
+CartRouter.get('/addToCart', CartController.addToCart);
+// ? [GET] /getCart
+CartRouter.get('/getMiniCart', CartController.getCart);
+
+// ? [GET] /removeMiniCart
+CartRouter.get('/removeMiniCart/:id', CartController.deleteItemCart);
 
 module.exports = CartRouter;
